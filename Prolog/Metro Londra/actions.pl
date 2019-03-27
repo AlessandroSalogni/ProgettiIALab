71e3e3c231722d7e7b339d6fd1e4ebb068258d14
AlessandroSalogni <20013907@studenti.uniupo.it>
2019-03-27 22:45:23 +0100

    Separate azioni dalla descrizione della metro

---
+++ Prolog/Metro Londra/actions.pl
@@ -0,0 +1,26 @@
+% stato: [at(Stazione), Location]
+% Location può essere in(NomeLinea, Dir) o
+%  'ground' se l'agente non è su nessun treno
+% Dir può esere 0 o 1
+
+% Azioni:
+%  sali(Linea, Dir)
+%  scendi(Stazione)
+%  vai(Linea, Dir, StazionePartenza, StazioneArrivo)
+
+applicabile(sali(Linea,Dir),[at(Stazione),ground]) :- fermata(Stazione,Linea), member(Dir,[0,1]).
+applicabile(scendi(Stazione),[at(Stazione),in(_,_)]).
+applicabile(vai(Linea,Dir,SP,SA),[at(SP),in(Linea,Dir)]) :- tratta(Linea,Dir,SP,SA).
+
+trasforma(sali(Linea,Dir),[at(Stazione),ground],[at(Stazione),in(Linea,Dir)]).
+trasforma(scendi(Stazione),[at(Stazione),in(_,_)],[at(Stazione),ground]).
+trasforma(vai(Linea,Dir,SP,SA),[at(SP),in(Linea,Dir)],[at(SA),in(Linea,Dir)]) :- tratta(Linea,Dir,SP,SA).
+
+costo(sali(_,_), 4).
+costo(scendi(_), 1).
+costo(vai(_,_,SP,SA), Costo) :-
+  stazione(SP, SPx, SPy),
+  stazione(SA, SAx, SAy),
+  Temp is ((SPx - SAx) ^ 2) + ((SPy - SAy) ^ 2),
+  Distanza is sqrt(Temp),
+  Costo is (Distanza * 625 / 10) / 60. % Sistemare % 10m/s
--- Prolog/Metro Londra/tube06-07.pl
+++ Prolog/Metro Londra/tube06-07.pl
@@ -1,33 +1,3 @@
-% stato: [at(Stazione), Location]
-% Location può essere in(NomeLinea, Dir) o
-%  'ground' se l'agente non è su nessun treno
-% Dir può esere 0 o 1
-
-
-% Azioni:
-%  sali(Linea, Dir)
-%  scendi(Stazione)
-%  vai(Linea, Dir, StazionePartenza, StazioneArrivo)
-
-applicabile(sali(Linea,Dir),[at(Stazione),ground]) :- fermata(Stazione,Linea), member(Dir,[0,1]).
-applicabile(scendi(Stazione),[at(Stazione),in(_,_)]).
-applicabile(vai(Linea,Dir,SP,SA),[at(SP),in(Linea,Dir)]) :- tratta(Linea,Dir,SP,SA).
-
-trasforma(sali(Linea,Dir),[at(Stazione),ground],[at(Stazione),in(Linea,Dir)]).
-trasforma(scendi(Stazione),[at(Stazione),in(_,_)],[at(Stazione),ground]).
-trasforma(vai(Linea,Dir,SP,SA),[at(SP),in(Linea,Dir)],[at(SA),in(Linea,Dir)]) :- tratta(Linea,Dir,SP,SA).
-
-uguale(S,S).
-
-costo(sali(_,_), 4).
-costo(scendi(_), 1).
-costo(vai(_,_,SP,SA), Costo) :-
-  stazione(SP, SPx, SPy),
-  stazione(SA, SAx, SAy),
-  Temp is ((SPx - SAx) ^ 2) + ((SPy - SAy) ^ 2),
-  Distanza is sqrt(Temp),
-  Costo is (Distanza * 625 / 10) / 60. % Sistemare % 10m/s
-
 % percorso(Linea, Dir, ListaFermate)
 
 percorso(piccadilly,0,['Kings Cross','Holborn','Covent Garden','Leicester Square','Piccadilly Circus','Green Park','South Kensington','Gloucester Road','Earls Court']).
@@ -79,7 +49,6 @@ stazione('Westminster',8,1.8).
 fermata(Stazione,Linea) :- percorso(Linea,0,P), member(Stazione,P).
 
 
-%iniziale([at('Bayswater'),ground]).
-iniziale([at('London Bridge'),ground]).
-finale([at('Earls Court'),ground]).
-%finale([at('Piccadilly Circus'),ground]).
+iniziale([at('Bayswater'),ground]).
+
+finale([at('Piccadilly Circus'),ground]).
