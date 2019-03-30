50807946c309ca467e5ee7ae54bf3d9607dba11a
AlessandroSalogni <20013907@studenti.uniupo.it>
2019-03-30 12:34:32 +0100

    Modificato calcolo del costo

--- Prolog/Metro Londra/actions.pl
+++ Prolog/Metro Londra/actions.pl
@@ -21,6 +21,7 @@ costo(scendi(_), 1).
 costo(vai(_,_,SP,SA), Costo) :-
   stazione(SP, SPx, SPy),
   stazione(SA, SAx, SAy),
-  Temp is ((SPx - SAx) ^ 2) + ((SPy - SAy) ^ 2),
-  Distanza is sqrt(Temp),
-  Costo is (Distanza * 625 / 10) / 60. % Sistemare % 10m/s
+  P is 0.017453292519943295,
+  A is (0.5 - cos((SAy - SPy) * P) / 2 + cos(SPy * P) * cos(SAy * P) * (1 - cos((SAx - SPx) * P)) / 2),
+  Dis is (12742 * asin(sqrt(A))),
+  Costo is (Dis * 1000 / 10) / 60. % (secondi) /60 -> minuti
