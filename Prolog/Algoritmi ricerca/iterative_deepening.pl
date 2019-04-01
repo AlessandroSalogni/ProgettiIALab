ed1218562e8d3b60830a08450e22ec16c8e24910
perottiriccardo <20013787@studenti.uniupo.it>
2019-04-01 10:41:58 +0200

    Aggiunta di Stato start nei visitati.

--- Prolog/Algoritmi ricerca/iterative_deepening.pl
+++ Prolog/Algoritmi ricerca/iterative_deepening.pl
@@ -1,8 +1,9 @@
 iterative_deepening(Soluzione) :-
   iniziale(S),
-  id_limit(S, Soluzione, [], 0).
+  id_limit(S, Soluzione, [S], 0).
 
-id_limit(S, Soluzione, Visitati, Soglia) :- dfs_limit(S, Soluzione, Visitati, Soglia), !.
+id_limit(S, Soluzione, Visitati, Soglia) :- 
+  dfs_limit(S, Soluzione, Visitati, Soglia), !.
 id_limit(S, Soluzione, Visitati, Soglia) :-
   NuovaSoglia is Soglia + 1,
   id_limit(S, Soluzione, Visitati, NuovaSoglia).
