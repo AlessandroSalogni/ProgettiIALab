5964958d84b4e8eadec3ce099fd7fa34b43c21b1
perottiriccardo <20013787@studenti.uniupo.it>
2019-04-01 11:13:15 +0200

    Minor change

--- Prolog/Algoritmi ricerca/utilities/priority_queue.pl
+++ Prolog/Algoritmi ricerca/utilities/priority_queue.pl
@@ -1,12 +1,12 @@
-coda_priorita(Coda, [], Coda).
-coda_priorita(Coda, [Nodo|AltriFigli], NuovaCoda) :-
-  inserisci_nodo(Coda, Nodo, CodaConNodo),
-  coda_priorita(CodaConNodo, AltriFigli, NuovaCoda).
+priority_queue(Coda, [], Coda).
+priority_queue(Coda, [Nodo|AltriFigli], NuovaCoda) :-
+  insert_node(Coda, Nodo, CodaConNodo),
+  priority_queue(CodaConNodo, AltriFigli, NuovaCoda).
 
-inserisci_nodo([], Nodo, [Nodo]).
-inserisci_nodo([nodo(SCoda, AzioniPerSCoda, CostoAzioniPerSCoda, EuristicaDaSCoda)|TailCoda], 
+insert_node([], Nodo, [Nodo]).
+insert_node([nodo(SCoda, AzioniPerSCoda, CostoAzioniPerSCoda, EuristicaDaSCoda)|TailCoda], 
 nodo(SNodo, AzioniPerSNodo, CostoAzioniPerSNodo, EuristicaDaSNodo),
 [nodo(SCoda, AzioniPerSCoda, CostoAzioniPerSCoda, EuristicaDaSCoda)|NuovaCoda]) :-
   CostoAzioniPerSCoda + EuristicaDaSCoda =< CostoAzioniPerSNodo + EuristicaDaSNodo, !,
-  inserisci_nodo(TailCoda, nodo(SNodo, AzioniPerSNodo, CostoAzioniPerSNodo, EuristicaDaSNodo), NuovaCoda).
-inserisci_nodo(Coda, Nodo, [Nodo|Coda]).
+  insert_node(TailCoda, nodo(SNodo, AzioniPerSNodo, CostoAzioniPerSNodo, EuristicaDaSNodo), NuovaCoda).
+insert_node(Coda, Nodo, [Nodo|Coda]).
