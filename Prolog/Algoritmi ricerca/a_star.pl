166128361cdb255ae47fdd8dec4800be8a014250
AlessandroSalogni <20013907@studenti.uniupo.it>
2019-04-01 21:42:47 +0200

    Minor change

--- Prolog/Algoritmi ricerca/a_star.pl
+++ Prolog/Algoritmi ricerca/a_star.pl
@@ -2,19 +2,19 @@ a_star(Soluzione) :-
     iniziale(S),
     as([nodo(S, [], 0, 0)], Soluzione, 0). %euristica a 0?
 
-as([nodo(S, AzioniPerS, _, _)|_], AzioniPerS, Espansi) :- finale(S), !, write(Espansi), nl.
-as([nodo(S, AzioniPerS, CostoAzioniPerS, EuristicaDaS)|Tail], Soluzione, Espansi) :-
-  findall(Azione, applicabile(Azione, S), ListaAzioniApplicabili),
-  expand_children(nodo(S, AzioniPerS, CostoAzioniPerS, EuristicaDaS), ListaAzioniApplicabili, ListaFigli),
+as([nodo(S, AzPerS, _, _)|_], AzPerS, Espansi) :- finale(S), !, write(Espansi), nl.
+as([nodo(S, AzPerS, CostoAzPerS, EuristicaDaS)|Tail], Soluzione, Espansi) :-
+  findall(Azione, applicabile(Azione, S), ListaAzApplicabili),
+  expand_children(nodo(S, AzPerS, CostoAzPerS, EuristicaDaS), ListaAzApplicabili, ListaFigli),
   priority_queue(Tail, ListaFigli, NuovaCoda),
   NuovoEspansi is Espansi + 1,
   as(NuovaCoda, Soluzione, NuovoEspansi).
 
 expand_children(_, [], []).
-expand_children(nodo(S, AzioniPerS, CostoAzioniPerS, EuristicaDaS), [Azione|AltreAzioni],
-[nodo(S_Nuovo, [Azione|AzioniPerS], CostoAzioniPerSNuovo, EuristicaDaSNuovo)|FigliTail]) :-
+expand_children(nodo(S, AzPerS, CostoAzPerS, EuristicaDaS), [Azione|AltreAz],
+[nodo(S_Nuovo, [Azione|AzPerS], CostoAzPerSNuovo, EuristicaDaSNuovo)|FigliTail]) :-
   trasforma(Azione, S, S_Nuovo),
   costo(Azione, Costo),
-  CostoAzioniPerSNuovo is CostoAzioniPerS + Costo,
+  CostoAzPerSNuovo is CostoAzPerS + Costo,
   heuristic(S_Nuovo, EuristicaDaSNuovo),
-  expand_children(nodo(S, AzioniPerS, CostoAzioniPerS, EuristicaDaS), AltreAzioni, FigliTail).
+  expand_children(nodo(S, AzPerS, CostoAzPerS, EuristicaDaS), AltreAz, FigliTail).
