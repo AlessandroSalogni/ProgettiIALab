268a89a3ebdb96466086734a9bd0f6b59f783dfb
AlessandroSalogni <20013907@studenti.uniupo.it>
2019-04-01 22:04:12 +0200

    Aggiunta euristca a 0

--- Prolog/Algoritmi ricerca/uniform_cost.pl
+++ Prolog/Algoritmi ricerca/uniform_cost.pl
@@ -1,24 +1,24 @@
 uniform_cost(Soluzione) :-
   statistics(walltime, [TimeSinceStart | [TimeSinceLastCall]]),
   iniziale(S),
-  uc([nodo(S, [], 0)], Soluzione, [], 0),
+  uc([nodo(S, [], 0, 0)], Soluzione, [], 0),
   statistics(walltime, [NewTimeSinceStart | [ExecutionTime]]),
   write('Execution took '), write(ExecutionTime), write(' ms.'), nl.
 
-uc([nodo(S, AzPerS, CostoAzPerS)|_], AzPerS, _, Espansi) :- finale(S), /*!,*/ write(CostoAzPerS), nl, write(Espansi), nl.
-uc([nodo(S, AzPerS, CostoAzPerS)|Tail], Soluzione, Visitati, Espansi) :-
+uc([nodo(S, AzPerS, CostoAzPerS, _)|_], AzPerS, _, Espansi) :- finale(S), /*!,*/ write(CostoAzPerS), nl, write(Espansi), nl.
+uc([nodo(S, AzPerS, CostoAzPerS, _)|Tail], Soluzione, Visitati, Espansi) :-
   findall(Azione, applicabile(Azione, S), ListaAzApplicabili),
-  expand_children(nodo(S, AzPerS, CostoAzPerS), ListaAzApplicabili, Visitati, ListaFigli),
+  expand_children(nodo(S, AzPerS, CostoAzPerS, 0), ListaAzApplicabili, Visitati, ListaFigli),
   priority_queue(Tail, ListaFigli, NuovaCoda),
   NodiEspansi is Espansi + 1,
   uc(NuovaCoda, Soluzione, [S|Visitati], NodiEspansi).
 
 expand_children(_, [], _, []).
-expand_children(nodo(S, AzPerS, CostoAzPerS), [Azione|AltreAz], Visitati, [nodo(S_Nuovo, [Azione|AzPerS], CostoAzPerSNuovo)|FigliTail]) :-
+expand_children(nodo(S, AzPerS, CostoAzPerS, _), [Azione|AltreAz], Visitati, [nodo(S_Nuovo, [Azione|AzPerS], CostoAzPerSNuovo, 0)|FigliTail]) :-
   trasforma(Azione, S, S_Nuovo),
   \+member(S_Nuovo, Visitati), !,
   costo(Azione, Costo),
   CostoAzPerSNuovo is CostoAzPerS + Costo,
-  expand_children(nodo(S, AzPerS, CostoAzPerS), AltreAz, Visitati, FigliTail).
+  expand_children(nodo(S, AzPerS, CostoAzPerS, 0), AltreAz, Visitati, FigliTail).
 expand_children(Nodo, [_|AltreAz], Visitati, FigliTail) :- % Viene eseguito quando not member fallisce perchè non è stato eseguito il cut
   expand_children(Nodo, AltreAz, Visitati, FigliTail).
