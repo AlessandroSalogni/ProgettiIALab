062ce920c0d35a0c64cd0737b2b7885dad79b6f9
perottiriccardo <20013787@studenti.uniupo.it>
2019-04-03 10:41:16 +0200

    Inizio gestione visitati

--- Prolog/Algoritmi ricerca/a_star.pl
+++ Prolog/Algoritmi ricerca/a_star.pl
@@ -1,23 +1,30 @@
 a_star(Soluzione) :-
   statistics(walltime, [TimeSinceStart | [TimeSinceLastCall]]),
   iniziale(S),
-  as([nodo(S, [], 0, 0)], Soluzione, 0), %euristica a 0?
+  as([nodo(S, [], 0, 0)], Soluzione, [], 0), %euristica a 0?
   statistics(walltime, [NewTimeSinceStart | [ExecutionTime]]),
   write('Execution took '), write(ExecutionTime), write(' ms.'), nl.
 
-as([nodo(S, AzPerS, CostoAzPerS, _)|_], AzPerS, Espansi) :- finale(S), write(CostoAzPerS), nl, !, write(Espansi), nl.
-as([nodo(S, AzPerS, CostoAzPerS, EuristicaDaS)|Tail], Soluzione, Espansi) :-
+as([nodo(S, AzPerS, CostoAzPerS, _)|_], AzPerS, _, Espansi) :- finale(S), write(CostoAzPerS), nl, !, write(Espansi), nl.
+as([nodo(S, AzPerS, CostoAzPerS, EuristicaDaS)|Tail], Soluzione, Visitati, Espansi) :-
   findall(Azione, applicabile(Azione, S), ListaAzApplicabili),
-  expand_children(nodo(S, AzPerS, CostoAzPerS, EuristicaDaS), ListaAzApplicabili, ListaFigli),
+  expand_children(nodo(S, AzPerS, CostoAzPerS, EuristicaDaS), ListaAzApplicabili, Visitati, ListaFigli),
   priority_queue(Tail, ListaFigli, NuovaCoda),
   NuovoEspansi is Espansi + 1,
-  as(NuovaCoda, Soluzione, NuovoEspansi).
+  as(NuovaCoda, Soluzione, [S|Visitati], NuovoEspansi).
 
-expand_children(_, [], []).
-expand_children(nodo(S, AzPerS, CostoAzPerS, EuristicaDaS), [Azione|AltreAz],
+expand_children(_, [], _, []).
+expand_children(nodo(S, AzPerS, CostoAzPerS, EuristicaDaS), [Azione|AltreAz], Visitati,
 [nodo(S_Nuovo, [Azione|AzPerS], CostoAzPerSNuovo, EuristicaDaSNuovo)|FigliTail]) :-
   trasforma(Azione, S, S_Nuovo),
+  \+member(S_Nuovo, Visitati), !,
   costo(Azione, Costo),
   CostoAzPerSNuovo is CostoAzPerS + Costo,
   heuristic(S_Nuovo, EuristicaDaSNuovo),
-  expand_children(nodo(S, AzPerS, CostoAzPerS, EuristicaDaS), AltreAz, FigliTail).
+  expand_children(nodo(S, AzPerS, CostoAzPerS, EuristicaDaS), AltreAz, Visitati, FigliTail).
+expand_children(nodo(S, AzPerS, CostoAzPerS, EuristicaDaS), [Azione|AltreAz], Visitati, FigliTail) :- % Viene eseguito quando not member fallisce perchè non è stato eseguito il cut
+  %trasforma(Azione, S, S_Nuovo),
+  %find_node_visitated(S_Nuovo, Visitati, NodoVisitato),
+  expand_children(Nodo, AltreAz, Visitati, FigliTail).
+
+%find_node_visitated(S_Nuovo, [HeadVisitati | TailVisitati], NodoVisitato) :- .
