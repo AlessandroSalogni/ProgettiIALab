712cc2200937cb2a9006149ced4b84582e58e59e
AlessandroSalogni <20013907@studenti.uniupo.it>
2019-04-04 16:40:46 +0200

    Sistemato nomi variabili

--- Prolog/Algoritmi ricerca/a_star.pl
+++ Prolog/Algoritmi ricerca/a_star.pl
@@ -1,32 +1,29 @@
 a_star(Soluzione) :-
   statistics(walltime, [TimeSinceStart | [TimeSinceLastCall]]),
   iniziale(S),
-  as([nodo(S, [], 0, 0)], Soluzione, [], 0), %euristica a 0?
+  as([nodo(S, [], 0, 0)], Soluzione, [], 0), %Eur a 0?
   statistics(walltime, [NewTimeSinceStart | [ExecutionTime]]),
   write('Execution took '), write(ExecutionTime), write(' ms.'), nl.
 
 as([nodo(S, AzPerS, CostoAzPerS, _)|_], AzPerS, _, Espansi) :- finale(S), write(CostoAzPerS), nl, !, write(Espansi), nl.
-as([nodo(S, AzPerS, CostoAzPerS, EuristicaDaS)|Tail], Soluzione, Visitati, Espansi) :-
-  findall(Azione, applicabile(Azione, S), ListaAzApplicabili),
-  expand_children(nodo(S, AzPerS, CostoAzPerS, EuristicaDaS), ListaAzApplicabili, Visitati, ListaFigli),
+as([nodo(S, AzPerS, CostoAzPerS, EurDaS)|Tail], Soluzione, Vis, Espansi) :-
+  findall(Az, applicabile(Az, S), ListaAzApplicabili),
+  expand_children(nodo(S, AzPerS, CostoAzPerS, EurDaS), ListaAzApplicabili, Vis, ListaFigli),
   priority_queue(Tail, ListaFigli, NuovaCoda),
   NuovoEspansi is Espansi + 1,
-  as(NuovaCoda, Soluzione, [nodo(S, CostoAzPerS)|Visitati], NuovoEspansi).
+  as(NuovaCoda, Soluzione, [nodo(S, CostoAzPerS)|Vis], NuovoEspansi).
 
 expand_children(_, [], _, []).
-expand_children(nodo(S, AzPerS, CostoAzPerS, EuristicaDaS), [Azione|AltreAz], Visitati,
-[nodo(S_Nuovo, [Azione|AzPerS], CostoAzPerSNuovo, EuristicaDaSNuovo)|FigliTail]) :-
-  trasforma(Azione, S, S_Nuovo),
-  costo(Azione, Costo),
+expand_children(nodo(S, AzPerS, CostoAzPerS, EurDaS), [Az|AltreAz], Vis, [nodo(S_Nuovo, [Az|AzPerS], CostoAzPerSNuovo, EurDaSNuovo)|FigliTail]) :-
+  trasforma(Az, S, S_Nuovo),
+  costo(Az, Costo),
   CostoAzPerSNuovo is CostoAzPerS + Costo,
-  revisit_node(nodo(S_Nuovo, CostoAzPerSNuovo), Visitati, NuoviVisitati), !,
-  heuristic(S_Nuovo, EuristicaDaSNuovo),
-  expand_children(nodo(S, AzPerS, CostoAzPerS, EuristicaDaS), AltreAz, NuoviVisitati, FigliTail).
-expand_children(Nodo, [Azione|AltreAz], Visitati, FigliTail) :- % Viene eseguito quando not member fallisce perchè non è stato eseguito il cut
-  expand_children(Nodo, AltreAz, Visitati, FigliTail).
+  revisit_node(nodo(S_Nuovo, CostoAzPerSNuovo), Vis, NuoviVis), !,
+  heuristic(S_Nuovo, EurDaSNuovo),
+  expand_children(nodo(S, AzPerS, CostoAzPerS, EurDaS), AltreAz, NuoviVis, FigliTail).
+expand_children(Nodo, [Az|AltreAz], Vis, FigliTail) :- expand_children(Nodo, AltreAz, Vis, FigliTail).
 
 revisit_node(_, [], []).
-revisit_node(nodo(S, CostoAzPerS), [nodo(S, CostoAzPerSVisitato)|AltriVisitati], AltriVisitati) :-
-  !, CostoAzPerS < CostoAzPerSVisitato.
-revisit_node(nodo(S, CostoAzPerS), [nodo(SVisitato, CostoAzPerSVisitato)|AltriVisitati], [nodo(SVisitato, CostoAzPerSVisitato)|NuoviVisitati]) :-
-  revisit_node(nodo(S, CostoAzPerS), AltriVisitati, NuoviVisitati).
+revisit_node(nodo(S, CostoAzPerS), [nodo(S, CostoAzPerSVis)|AltriVis], AltriVis) :- !, CostoAzPerS < CostoAzPerSVis.
+revisit_node(nodo(S, CostoAzPerS), [nodo(SVis, CostoAzPerSVis)|AltriVis], [nodo(SVis, CostoAzPerSVis)|NuoviVis]) :-
+  revisit_node(nodo(S, CostoAzPerS), AltriVis, NuoviVis).
