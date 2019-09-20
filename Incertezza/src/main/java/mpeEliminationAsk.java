993063bfcc178b6e9abbfd42a6a4df17c709b624
AlessandroSalogni <20013907@studenti.uniupo.it>
2019-09-20 12:40:32 +0200

    Aggiunta classe per mpe vuota

---
+++ Incertezza/src/main/java/mpeEliminationAsk.java
@@ -0,0 +1,11 @@
+import aima.core.probability.CategoricalDistribution;
+import aima.core.probability.RandomVariable;
+import aima.core.probability.bayes.BayesInference;
+import aima.core.probability.bayes.BayesianNetwork;
+import aima.core.probability.proposition.AssignmentProposition;
+
+public class mpeEliminationAsk implements BayesInference {
+    public CategoricalDistribution ask(RandomVariable[] X, AssignmentProposition[] e, BayesianNetwork bn) {
+        return null;
+    }
+}
