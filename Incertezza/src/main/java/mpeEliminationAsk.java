086865a4e42014c89f6cd4c806ad9ce743247178
perottiriccardo <20013787@studenti.uniupo.it>
2019-09-21 11:44:21 +0200

    Importato parser di reti bayesiane .bif Creata cartella di risorse Aggiornato pom.xml

--- Incertezza/src/main/java/mpeEliminationAsk.java
+++ Incertezza/src/main/java/mpeEliminationAsk.java
@@ -2,10 +2,27 @@ import aima.core.probability.CategoricalDistribution;
 import aima.core.probability.RandomVariable;
 import aima.core.probability.bayes.BayesInference;
 import aima.core.probability.bayes.BayesianNetwork;
+import aima.core.probability.bayes.Node;
+import aima.core.probability.bayes.impl.FullCPTNode;
 import aima.core.probability.proposition.AssignmentProposition;
+import bifparser.BifBNReader;
 
 public class mpeEliminationAsk implements BayesInference {
     public CategoricalDistribution ask(RandomVariable[] X, AssignmentProposition[] e, BayesianNetwork bn) {
         return null;
     }
+
+    /*USO del parser
+
+    public static void main(String[] args) throws Exception {
+        BifBNReader bnReader = new BifBNReader("earthquake.bif") {
+            @Override
+            protected Node nodeCreation(RandomVariable var, double[] probs, Node... parents) {
+                return new FullCPTNode(var, probs, parents);
+            }
+        };
+
+        BayesianNetwork bn = bnReader.getBayesianNetwork();
+    }
+    */
 }
