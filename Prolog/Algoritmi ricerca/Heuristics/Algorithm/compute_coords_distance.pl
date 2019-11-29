bfcd1f773b568b9e71fee0752014aeebaed4002e
AlessandroSalogni <20013907@studenti.uniupo.it>
2019-11-29 15:14:19 +0100

    Commentato calcolo distanza in linea d'aria

--- Prolog/Algoritmi ricerca/Heuristics/Algorithm/compute_coords_distance.pl
+++ Prolog/Algoritmi ricerca/Heuristics/Algorithm/compute_coords_distance.pl
@@ -1,7 +1,8 @@
 coords_distance(SP, SA, Costo) :-
   stazione(SP, SPx, SPy),
   stazione(SA, SAx, SAy),
-  P is 0.017453292519943295,
+  P is 0.017453292519943295, %Pi/180
   A is (0.5 - cos((SAy - SPy) * P) / 2 + cos(SPy * P) * cos(SAy * P) * (1 - cos((SAx - SPx) * P)) / 2),
-  Dis is (12742 * asin(sqrt(A))),
-  Costo is (Dis * 1000 / 10) / 60. % (secondi) /60 -> minuti
+  DisKM is (2 * 6371 * asin(sqrt(A))), % R = 6371 = raggio Terra in KM
+  DisM is DisKM * 1000,
+  Costo is (DisM / 10) / 60. % (Dis in m / 10m/s (36 km/h)) -> (costo in secondi) / 60 -> costo in minuti
