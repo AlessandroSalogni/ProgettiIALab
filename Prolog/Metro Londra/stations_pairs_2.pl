a95b1709d59638971e032291f8ff3185dd6c630d
Riccardo Perotti <32493411+perottiriccardo@users.noreply.github.com>
2019-12-06 13:21:28 +0100

    Coppie zona 2

---
+++ Prolog/Metro Londra/stations_pairs_2.pl
@@ -0,0 +1,179 @@
+/* Bakerloo */
+coppie_stazioni("Kensal Green", "Queen's Park", 0).
+coppie_stazioni("Queen's Park", "Kilburn Park", 0).
+coppie_stazioni("Kilburn Park", "Maida Vale", 0).
+coppie_stazioni("Maida Vale", "Warwick Avenue", 0).
+coppie_stazioni("Warwick Avenue", "Paddington", 0).
+coppie_stazioni("Edgware Road", "Marylebone", 0).
+coppie_stazioni("Marylebone", "Baker Street", 0).
+coppie_stazioni("Baker Street", "Regent's Park", 0).
+coppie_stazioni("Regent's Park", "Oxford Circus", 0).
+coppie_stazioni("Oxford Circus", "Piccadilly Circus", 0).
+coppie_stazioni("Piccadilly Circus", "Charing Cross", 0).
+coppie_stazioni("Waterloo", "Lambeth North", 0).
+coppie_stazioni("Lambeth North", "Elephant & Castle", 0).
+
+/* Central */
+coppie_stazioni("North Acton", "East Acton", 0).
+coppie_stazioni("East Acton", "White City", 0).
+coppie_stazioni("White City", "Shepherd's Bush", 0).
+coppie_stazioni("Shepherd's Bush", "Holland Park", 0).
+coppie_stazioni("Holland Park", "Notting Hill Gate", 0).
+coppie_stazioni("Notting Hill Gate", "Queensway", 0).
+coppie_stazioni("Queensway", "Lancaster Gate", 0).
+coppie_stazioni("Lancaster Gate", "Marble Arch", 0).
+coppie_stazioni("Marble Arch", "Bond Street", 0).
+coppie_stazioni("Bond Street", "Oxford Circus", 0).
+coppie_stazioni("Oxford Circus", "Tottenham Court Road", 0).
+coppie_stazioni("Tottenham Court Road", "Holborn", 0).
+coppie_stazioni("Holborn", "Chancery Lane", 0).
+coppie_stazioni("Chancery Lane", "St. Paul's", 0).
+coppie_stazioni("St. Paul's", "Bank", 0).
+coppie_stazioni("Bank", "Liverpool Street", 0).
+
+/* Victoria*/
+coppie_stazioni("Finsbury Park", "Highbury & Islington", 0).
+coppie_stazioni("Highbury & Islington", "King's Cross St. Pancras", 0).
+coppie_stazioni("Warren Street", "Oxford Circus", 0).
+coppie_stazioni("Oxford Circus", "Green Park", 0).
+coppie_stazioni("Green Park", "Victoria", 0).
+coppie_stazioni("Victoria", "Pimlico", 0).
+coppie_stazioni("Pimlico", "Vauxhall", 0).
+coppie_stazioni("Vauxhall", "Stockwell", 0).
+coppie_stazioni("Stockwell", "Brixton", 0).
+
+/* Waterloo & City */
+coppie_stazioni("Waterloo", "Bank", 0).
+
+/* Jubilee */
+coppie_stazioni("Willesden Green", "Kilburn", 0).
+coppie_stazioni("Kilburn", "West Hampstead", 0).
+coppie_stazioni("West Hampstead", "Finchley Road", 0).
+coppie_stazioni("Finchley Road", "Swiss Cottage", 0).
+coppie_stazioni("Swiss Cottage", "St. John's Wood", 0).
+coppie_stazioni("St. John's Wood", "Baker Street", 0).
+coppie_stazioni("Baker Street", "Bond Street", 0).
+coppie_stazioni("Bond Street", "Green Park", 0).
+coppie_stazioni("Green Park", "Westminster", 0).
+coppie_stazioni("Westminster", "Waterloo", 0).
+coppie_stazioni("Waterloo", "Southwark", 0).
+coppie_stazioni("Southwark", "London Bridge", 0).
+coppie_stazioni("London Bridge", "Bermondsey", 0).
+coppie_stazioni("Bermondsey", "Canada Water", 0).
+coppie_stazioni("Canada Water", "Canary Wharf", 0).
+coppie_stazioni("Canary Wharf", "North Greenwich", 0).
+
+/* Northern */
+coppie_stazioni("Hampstead", "Belsize Park", 0).
+coppie_stazioni("Belsize Park", "Chalk Farm", 0).
+coppie_stazioni("Chalk Farm", "Camden Town", 0).
+coppie_stazioni("Archway", "Tufnell Park", 0).
+coppie_stazioni("Tufnell Park", "Kentish Town", 0).
+coppie_stazioni("Kentish Town", "Camden Town", 0).
+coppie_stazioni("Camden Town", "Mornington Crescent", 0).
+coppie_stazioni("Mornington Crescent", "Euston", 0).
+coppie_stazioni("Euston", "Warren Street", 0).
+coppie_stazioni("Warren Street", "Goodge Street", 0).
+coppie_stazioni("Goodge Street", "Tottenham Court Road", 0).
+coppie_stazioni("Tottenham Court Road", "Leicester Square", 0).
+coppie_stazioni("Leicester Square", "Charing Cross", 0).
+coppie_stazioni("Charing Cross", "Embankment", 0).
+coppie_stazioni("Embankment", "Waterloo", 0).
+coppie_stazioni("Waterloo", "Kennington", 0).
+coppie_stazioni("Camden Town", "Euston", 0).
+coppie_stazioni("Euston", "King's Cross St. Pancras", 0).
+coppie_stazioni("King's Cross St. Pancras", "Angel", 0).
+coppie_stazioni("Angel", "Old Street", 0).
+coppie_stazioni("Old Street", "Moorgate", 0).
+coppie_stazioni("Moorgate", "Bank", 0).
+coppie_stazioni("Bank", "London Bridge", 0).
+coppie_stazioni("London Bridge", "Borough", 0).
+coppie_stazioni("Borough", "Elephant & Castle", 0).
+coppie_stazioni("Elephant & Castle", "Kennington", 0).
+coppie_stazioni("Kennington", "Oval", 0).
+coppie_stazioni("Oval", "Stockwell", 0).
+coppie_stazioni("Stockwell", "Clapham North", 0).
+coppie_stazioni("Clapham North", "Clapham Common", 0).
+coppie_stazioni("Clapham Common", "Clapham South", 0).
+
+/* Piccadilly */
+coppie_stazioni("Barons Court", "Earl's Court", 0).
+coppie_stazioni("South Kensington", "Knightsbridge", 0).
+coppie_stazioni("Knightsbridge", "Hyde Park Corner", 0).
+coppie_stazioni("Hyde Park Corner", "Green Park", 0).
+coppie_stazioni("Green Park", "Piccadilly Circus", 0).
+coppie_stazioni("Piccadilly Circus", "Leicester Square", 0).
+coppie_stazioni("Leicester Square", "Covent Garden", 0).
+coppie_stazioni("Covent Garden", "Holborn", 0).
+coppie_stazioni("Holborn", "Russell Square", 0).
+coppie_stazioni("Russell Square", "King's Cross St. Pancras", 0).
+coppie_stazioni("King's Cross St. Pancras", "Caledonian Road", 0).
+coppie_stazioni("Caledonian Road", "Holloway Road", 0).
+coppie_stazioni("Holloway Road", "Arsenal", 0).
+coppie_stazioni("Arsenal", "Finsbury Park", 0).
+coppie_stazioni("Finsbury Park", "Manor House", 0).
+
+/* Metropolitan */
+coppie_stazioni("Baker Street", "Finchley Road", 0).
+
+/* Hammersmith & City */
+coppie_stazioni("Hammersmith", "Goldhawk Road", 0).
+coppie_stazioni("Goldhawk Road", "Shepherd's Bush Market", 0).
+coppie_stazioni("Shepherd's Bush Market", "Latimer Road", 0).
+coppie_stazioni("Latimer Road", "Ladbroke Grove", 0).
+coppie_stazioni("Ladbroke Grove", "Westbourne Park", 0).
+coppie_stazioni("Westbourne Park", "Royal Oak", 0).
+coppie_stazioni("Royal Oak", "Paddington", 0).
+coppie_stazioni("Baker Street", "Great Portland Street", 0).
+coppie_stazioni("Great Portland Street", "Euston Square", 0).
+coppie_stazioni("Euston Square", "King's Cross St. Pancras", 0).
+coppie_stazioni("King's Cross St. Pancras", "Farringdon", 0).
+coppie_stazioni("Farringdon", "Barbican", 0).
+coppie_stazioni("Barbican", "Moorgate", 0).
+coppie_stazioni("Moorgate", "Liverpool Street", 0).
+coppie_stazioni("Liverpool Street", "Aldgate East", 0).
+
+/* Circle */
+coppie_stazioni("High Street Kensington", "Gloucester Road", 0).
+coppie_stazioni("Tower Hill", "Aldgate", 0).
+coppie_stazioni("Aldgate", "Liverpool Street", 0).
+coppie_stazioni("Baker Street", "Edgware Road", 0).
+
+/* District */
+coppie_stazioni("Bromley By Bow", "Bow Road", 0).
+coppie_stazioni("Bow Road", "Mile End", 0).
+coppie_stazioni("Mile End", "Stepney Green", 0).
+coppie_stazioni("Stepney Green", "Whitechapel", 0).
+coppie_stazioni("Whitechapel", "Aldgate East", 0).
+coppie_stazioni("Aldgate East", "Tower Hill", 0).
+coppie_stazioni("Tower Hill", "Monument", 0).
+coppie_stazioni("Monument", "Cannon Street", 0).
+coppie_stazioni("Cannon Street", "Mansion House", 0).
+coppie_stazioni("Mansion House", "Blackfriars", 0).
+coppie_stazioni("Blackfriars", "Temple", 0).
+coppie_stazioni("Temple", "Embankment", 0).
+coppie_stazioni("Embankment", "Westminster", 0).
+coppie_stazioni("Westminster", "St. James's Park", 0).
+coppie_stazioni("St. James's Park", "Victoria", 0).
+coppie_stazioni("Victoria", "Sloane Square", 0).
+coppie_stazioni("Sloane Square", "South Kensington", 0).
+coppie_stazioni("South Kensington", "Gloucester Road", 0).
+coppie_stazioni("Gloucester Road", "Earl's Court", 0).
+coppie_stazioni("Edgware Road", "Paddington", 0).
+coppie_stazioni("Paddington", "Bayswater", 0).
+coppie_stazioni("Bayswater", "Notting Hill Gate", 0).
+coppie_stazioni("Notting Hill Gate", "High Street Kensington", 0).
+coppie_stazioni("High Street Kensington", "Earl's Court", 0).
+coppie_stazioni("Earl's Court", "West Brompton", 0).
+coppie_stazioni("West Brompton", "Fulham Broadway", 0).
+coppie_stazioni("Fulham Broadway", "Parsons Green", 0).
+coppie_stazioni("Parsons Green", "Putney Bridge", 0).
+coppie_stazioni("Putney Bridge", "East Putney", 0).
+coppie_stazioni("Earl's Court", "West Kensington", 0).
+coppie_stazioni("West Kensington", "Barons Court", 0).
+coppie_stazioni("Barons Court", "Hammersmith", 0).
+coppie_stazioni("Hammersmith", "Ravenscourt Park", 0).
+coppie_stazioni("Ravenscourt Park", "Stamford Brook", 0).
+coppie_stazioni("Stamford Brook", "Turnham Green", 0).
+
+coppie_stazioni(SP, SA, 1) :- coppie_stazioni(SA, SP, 0).
